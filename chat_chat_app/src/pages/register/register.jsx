2f1d1434ae0275f6b3e1e77f1cda65c683567c18
hendmissonen <hendmissonen@gmail.com>
2023-11-30 19:36:23 +0200

    backend logout page and add  usestate in registerpage-frontend

--- chat_chat_app/src/pages/register/register.jsx
+++ chat_chat_app/src/pages/register/register.jsx
@@ -1,6 +1,11 @@
 import "./register.css";
 
 const Register = () => {
+  const [inputs, setInputs] = useState({
+    username: "",
+    email: "",
+    name: "",
+  });
   return <div className="register">Register</div>;
 };
 
