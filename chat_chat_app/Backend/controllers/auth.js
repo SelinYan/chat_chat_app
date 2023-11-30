1e11e9b3fc2c9da17494defacc19e1e9e784d927
hendmissonen <hendmissonen@gmail.com>
2023-11-30 17:37:53 +0200

    new update

--- chat_chat_app/Backend/controllers/auth.js
+++ chat_chat_app/Backend/controllers/auth.js
@@ -1,3 +1,4 @@
+import { response } from "express";
 import { db } from "../connect.js";
 import bycrypt from "bcryptjs";
 
@@ -5,7 +6,7 @@ export const register = (req, res) => {
   const q = "SELECT * FROM users WHERE username =?";
   db.query(q, [req.body.username], (err, data) => {
     if (err) return res.status(500).send(err);
-    if (data.length) return res.status(409).json("usr already exists");
+    if (data.length) return res.status(409).json("user already exists");
     //create a new user
     //to hash the ps we need to generate salt and it will get us ecrypted ps
     const salt = bycrypt.genSaltSync(10);
@@ -28,8 +29,17 @@ export const register = (req, res) => {
 
 export const login = (req, res) => {
   const q = "SELECT * FROM users WHERE username = ?";
+  db.query(q, [req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.length === 0) return res.status(404).json("user not found");
 
-  db;
+    const checkPassword = bycrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+    if (!checkPassword)
+      return response.status(400).json("wrong password or usernames");
+  });
 };
 
 export const logout = (req, res) => {};
--- chat_chat_app/Backend/index.js
+++ chat_chat_app/Backend/index.js
@@ -5,8 +5,12 @@ import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import commentRoutes from "./routes/comments.js";
 import likeRoutes from "./routes/likes.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 app.use("/Backend/authentication", authenticationRoutes);
 app.use("/Backend/users", userRoutes);
