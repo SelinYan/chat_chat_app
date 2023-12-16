c1aa39b8abe1c492613e93c78a91f203f75d3d69
Wen Yan <selinyan2016@gmail.com>
2023-12-16 12:02:29 +0200

    add leftBar and rightBar

---
+++ chat_chat_app/src/components/Rightbar.jsx
@@ -0,0 +1,210 @@
+const RightBar = () => {
+  return (
+    <div className="rightBar">
+      <div className="rightbar-container">
+        <div className="rightbar-item">
+          <div className="seemore-div">
+            <p className="rightbar-span">You might like</p>
+            <a href="">See more</a>
+          </div>
+          <hr className="rightbar-hr" />
+          <div className="rightbar-user">
+            <div className="rightbar-userInfo">
+              <img
+                className="rightbar-img"
+                src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg"
+                alt=""
+              />
+              <p className="rightbar-span">Emily Johnson</p>
+            </div>
+            <div className="rightbar-buttons">
+              <button>Add</button>
+              <button>Ignore</button>
+            </div>
+          </div>
+        </div>
+        <div className="rightbar-item">
+          <span className="rightbar-span">Latest Activities</span>
+          <div className="rightbar-user">
+            <div className="rightbar-userInfo">
+              <img
+                className="rightbar-img"
+                src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg"
+                alt=""
+              />
+              <p className="rightbar-para">
+                <span className="rightbar-span">Emily Johnson</span> changed
+                their cover picture
+              </p>
+            </div>
+          </div>
+          <div className="rightbar-user">
+            <div className="rightbar-userInfo">
+              <img
+                className="rightbar-img"
+                src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg"
+                alt=""
+              />
+              <p className="rightbar-para">
+                <span className="rightbar-span">Emily Johnson</span> changed
+                their cover picture
+              </p>
+            </div>
+          </div>
+          <div className="rightbar-user">
+            <div className="rightbar-userInfo">
+              <img
+                className="rightbar-img"
+                src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg"
+                alt=""
+              />
+              <p className="rightbar-para">
+                <span className="rightbar-span">Emily Johnson</span> shared a
+                video
+              </p>
+            </div>
+          </div>
+          <div className="rightbar-user">
+            <div className="rightbar-userInfo">
+              <img
+                className="rightbar-img"
+                src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg"
+                alt=""
+              />
+              <p className="rightbar-para">
+                <span className="rightbar-span">Ethan Anderson</span> changed
+                their profile picture
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="rightbar-item">
+          <span className="rightbar-span">Online Friends</span>
+          <div className="rightbar-user">
+            <div className="rightbar-userInfo">
+              <img
+                className="rightbar-img"
+                src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg"
+                alt=""
+              />
+              <div className="rightbar-online" />
+              <span className="rightbar-span">Olivia Thompson</span>
+            </div>
+          </div>
+          <div className="rightbar-user">
+            <div className="rightbar-userInfo">
+              <img
+                className="rightbar-img"
+                src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg"
+                alt=""
+              />
+              <div className="rightbar-online" />
+              <span className="rightbar-span">Liam Roberts</span>
+            </div>
+          </div>
+          <div className="rightbar-user">
+            <div className="rightbar-userInfo">
+              <img
+                className="rightbar-img"
+                src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg"
+                alt=""
+              />
+              <div className="rightbar-online" />
+              <span className="rightbar-span">Ava Mitchell</span>
+            </div>
+          </div>
+          <div className="rightbar-user">
+            <div className="rightbar-userInfo">
+              <img
+                className="rightbar-img"
+                src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg"
+                alt=""
+              />
+              <div className="rightbar-online" />
+              <span className="rightbar-span">Noah Turner</span>
+            </div>
+          </div>
+          <div className="rightbar-user">
+            <div className="rightbar-userInfo">
+              <img
+                className="rightbar-img"
+                src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg"
+                alt=""
+              />
+              <div className="rightbar-online" />
+              <span className="rightbar-span">Ava Mitchell</span>
+            </div>
+          </div>
+          <div className="rightbar-user">
+            <div className="rightbar-userInfo">
+              <img
+                className="rightbar-img"
+                src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg"
+                alt=""
+              />
+              <div className="rightbar-online" />
+              <span className="rightbar-span">Ava Mitchell</span>
+            </div>
+          </div>
+          <div className="rightbar-user">
+            <div className="rightbar-userInfo">
+              <img
+                className="rightbar-img"
+                src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg"
+                alt=""
+              />
+              <div className="rightbar-online" />
+              <span className="rightbar-span">Ava Mitchell</span>
+            </div>
+          </div>
+          <div className="rightbar-user">
+            <div className="rightbar-userInfo">
+              <img
+                className="rightbar-img"
+                src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg"
+                alt=""
+              />
+              <div className="rightbar-online" />
+              <span className="rightbar-span">Ava Mitchell</span>
+            </div>
+          </div>
+          <div className="rightbar-user">
+            <div className="rightbar-userInfo">
+              <img
+                className="rightbar-img"
+                src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg"
+                alt=""
+              />
+              <div className="rightbar-online" />
+              <span className="rightbar-span">Ava Mitchell</span>
+            </div>
+          </div>
+          <div className="rightbar-user">
+            <div className="rightbar-userInfo">
+              <img
+                className="rightbar-img"
+                src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg"
+                alt=""
+              />
+              <div className="rightbar-online" />
+              <span className="rightbar-span">Ava Mitchell</span>
+            </div>
+          </div>
+          <div className="rightbar-user">
+            <div className="rightbar-userInfo">
+              <img
+                className="rightbar-img"
+                src="https://images.pexels.com/photos/837358/pexels-photo-837358.jpeg"
+                alt=""
+              />
+              <div className="rightbar-online" />
+              <span className="rightbar-span">Ava Mitchell</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RightBar;
